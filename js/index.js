682c21e885c93e64a197e79ec9bda73e7a349909
NEEMIASAP <neemiasap@gmail.com>
2022-10-19 14:59:22 -0300

    done2

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 // Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
 
 
-// Iteration 3: Loops
 
 const hacker1 = "Jhon";
 
@@ -12,5 +9,47 @@ console.log(`The driver's name is ${hacker1}`);
 const hacker2 ="Colombo";
 
 console.log(`The navigator is ${hacker2}` );
+
+
+
+
+
+// Iteration 2: Conditionals
+
+
+if (hacker1.length > hacker2) { 
+
+    console.log (`The driver has the longest name, it has ${hacker1} characters.`);
+}
+
+else {
+
+if ( hacker1.length< hacker2.length)   {
+
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+
+}
+
+    else {
+
+        
+
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters !`);  
+
+
+
+}
+}
+
+
+
+
+
+
+
+
+
+// Iteration 3: Loops
+
 
 
