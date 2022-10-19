f72c4b5986ef502c187335b88bd465dfd13453bb
NEEMIASAP <neemiasap@gmail.com>
2022-10-19 14:08:16 -0300

    done1

--- js/index.js
+++ js/index.js
@@ -4,3 +4,13 @@
 
 
 // Iteration 3: Loops
+
+const hacker1 = "Jhon";
+
+console.log(`The driver's name is ${hacker1}`);
+
+const hacker2 ="Colombo";
+
+console.log(`The navigator is ${hacker2}` );
+
+
